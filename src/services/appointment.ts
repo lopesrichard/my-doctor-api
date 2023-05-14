c33f2c83584e7d9cac24a083a1c2e6099cbe0123
Richard Lopes <contato.richardlopes@outlook.com>
2023-05-14 17:03:35 -0300

    feat: add address on clinic queries

--- src/services/appointment.ts
+++ src/services/appointment.ts
@@ -5,18 +5,21 @@ import { AddAppointment, UpdateAppointment } from '../models';
 @Injectable()
 export class AppointmentService {
   async list(): Promise<Appointment[]> {
-    return await Appointment.find({ relations: { clinic: true, doctor: true, patient: true } });
+    return await Appointment.find({ relations: { clinic: { address: true }, doctor: true, patient: true } });
   }
 
   async get(id: number): Promise<Appointment | null> {
-    return await Appointment.findOne({ where: { id }, relations: { clinic: true, doctor: true, patient: true } });
+    return await Appointment.findOne({
+      where: { id },
+      relations: { clinic: { address: true }, doctor: true, patient: true },
+    });
   }
 
   async getByPatient(id: number): Promise<Appointment[]> {
     return await Appointment.find({
       where: { patient: { id } },
       order: { scheduledTo: -1 },
-      relations: { clinic: true, doctor: true },
+      relations: { clinic: { address: true }, doctor: true },
     });
   }
 
@@ -24,7 +27,7 @@ export class AppointmentService {
     return await Appointment.find({
       where: { doctor: { id } },
       order: { scheduledTo: -1 },
-      relations: { clinic: true, doctor: true },
+      relations: { clinic: { address: true }, doctor: true },
     });
   }
 
