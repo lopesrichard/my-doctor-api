fa57910635bcb3d4e61b9d795d4ce303ccd3d7f2
Richard Lopes <contato.richardlopes@outlook.com>
2023-05-14 17:03:56 -0300

    fix: cancel appointment http verb

--- src/controllers/appointment.ts
+++ src/controllers/appointment.ts
@@ -1,22 +1,12 @@
 import { AddAppointment, UpdateAppointment } from '../models';
 import { AppointmentStatus } from '../enums/appointment-status';
 
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppointmentService } from '../services/appointment';
 
 @Controller('appointments')
 export class AppointmentController {
   constructor(private readonly service: AppointmentService) {}
-
-  // @Get('doctors/:id/appointments')
-  // async getByDoctor(@Param('id') id: number) {
-  //   return await this.service.getByDoctor(id);
-  // }
-
-  // @Get('patients/:id/appointments')
-  // async getByPatient(@Param('id') id: number) {
-  //   return await this.service.getByPatient(id);
-  // }
 
   @Get()
   async list() {
@@ -34,7 +24,7 @@ export class AppointmentController {
     return await this.service.insert(data);
   }
 
-  @Put(':id/cancel')
+  @Delete(':id')
   async cancel(@Param('id') id: number) {
     const data: UpdateAppointment = { status: AppointmentStatus.CANCELED };
     return await this.service.update(id, data);
